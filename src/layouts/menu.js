eca820ab22087162655971e6abe40616a7804464
victormoya <vmoyacueto@gmail.com>
2017-10-06 01:18:10 +0200

    Add basic menu vertically on left

---
+++ src/layouts/menu.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const Menu = () => {
+  <div>
+    <nav>
+      <ul>
+        <li><a href="/">Welcome</a></li>
+        <li><a href="/listen">Listen</a></li>
+        <li><a href="/gigs">Attend</a></li>
+        <li><a href="/contact">Talk</a></li>
+      </ul>
+    </nav>
+  </div>
+}
+
+export default Menu
