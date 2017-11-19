3b7a8d9e2e437c6613f793116f610deb7d4e08fc
victormoya <vmoyacueto@gmail.com>
2017-11-19 12:50:54 +0100

    Change gigs widget now using songkick

--- src/pages/gigs.js
+++ src/pages/gigs.js
@@ -8,7 +8,8 @@ class Gigs extends React.Component {
     if(typeof document !== "undefined") {
       const script = document.createElement('script');
       script.type = 'text/javascript';
-      script.src = '//widget.bandsintown.com/main.min.js';
+      // script.src = '//widget.bandsintown.com/main.min.js';
+      script.src = '//widget.songkick.com/widget.js';
       script.async = true;
       document.body.appendChild(script);
     }
@@ -20,19 +21,13 @@ class Gigs extends React.Component {
         <Menu active="gigs"/>
         <div className="shows">
           <a
-            className="bit-widget-initializer"
-            data-artist-name="laverge"
-            data-language="en"
-            data-font="Concorde"
-            data-display-local-dates="false"
-            data-display-past-dates="true"
-            data-auto-style="false"
-            data-text-color="#FFFFFF"
-            data-link-color="#888888"
-            data-popup-background-color="rgba(0,0,0,0)"
-            data-background-color="rgba(30, 34, 46, 0.30)"
-            data-display-limit="15"
-            data-link-text-color="#FFFFFF">
+            href="http://www.songkick.com/artists/8348243"
+            className="songkick-widget"
+            data-theme="dark"
+            data-track-button="on"
+            data-font-color="#ffffff"
+            data-detect-style="true"
+            data-background-color="transparent">
           </a>
         </div>
       </div>
@@ -42,15 +37,18 @@ class Gigs extends React.Component {
 
 export default Gigs
 
-
 // <a
-//   href="http://www.songkick.com/artists/8348243"
-//   className="songkick-widget"
-//   data-theme="light"
-//   data-other-artists="on"
-//   data-track-button="on"
-//   data-detect-style="true"
-//   data-background-color="transparent">
-//   Laverge tour dates
+//   className="bit-widget-initializer"
+//   data-artist-name="laverge"
+//   data-language="en"
+//   data-font="Concorde"
+//   data-display-local-dates="false"
+//   data-display-past-dates="true"
+//   data-auto-style="false"
+//   data-text-color="#FFFFFF"
+//   data-link-color="#888888"
+//   data-popup-background-color="rgba(0,0,0,0)"
+//   data-background-color="rgba(30, 34, 46, 0.30)"
+//   data-display-limit="15"
+//   data-link-text-color="#FFFFFF">
 // </a>
-// <script src="https://widget.songkick.com/widget.js"></script>
