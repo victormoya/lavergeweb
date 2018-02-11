95439930f1e841473f021e19a0795f6df72a72a4
victormoya <vmoyacueto@gmail.com>
2018-02-11 21:50:15 +0100

    Add footer icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,23 +1,49 @@
 import React from 'react'
+import facebook from '../../static/media/icons/facebook.svg';
+import youtube from '../../static/media/icons/youtube3.svg';
+import instagram from '../../static/media/icons/instagram.svg';
+import spotify from '../../static/media/icons/spotify.svg';
+import bandcamp from '../../static/media/icons/bandcamp.svg';
+import apple from '../../static/media/icons/apple.svg';
 
 const Footer = (props) => (
   <div>
     <nav className="footer">
-      <ul>
-        <li>
-          <a target="_blank" href="/">FACEBOOK</a>
+      <ul className="social">
+        <li className="social__item">
+          <a target="_blank"
+             href="https://www.facebook.com/Lavergeband/"
+             title="Facebook">
+            <img src={facebook} alt="Facebook"/>
+          </a>
         </li>
-        <li>
-          <a target="_blank" href="/">YOUTUBE</a>
+        <li className="social__item">
+          <a target="_blank"
+             href="https://www.youtube.com/channel/UCoZ9-g-GvPzX52xxbkA6mmA"
+             title="YouTube">
+            <img src={youtube} alt="YouTube"/>
+          </a>
         </li>
-        <li>
-          <a target="_blank" href="/">INSTAGRAM</a>
+        <li className="social__item">
+          <a target="_blank"
+             href="https://www.instagram.com/laverge_band/"
+             title="Instagram">
+            <img src={instagram} alt="Instagram"/>
+          </a>
         </li>
-        <li>
-          <a target="_blank" href="/">SPOTIFY</a>
+        <li className="social__item">
+          <a target="_blank"
+             href="https://open.spotify.com/artist/7GcUN4O1EbffUOAij7xzND"
+             title="Spotify">
+            <img src={spotify} alt="Spotify"/>
+          </a>
         </li>
-        <li>
-          <a target="_blank" href="/">BANDCAMP</a>
+        <li className="social__item">
+          <a target="_blank"
+             href="https://itunes.apple.com/us/album/handle-this/1299818828"
+             title="Itunes">
+            <img src={apple} alt="Itunes"/>
+          </a>
         </li>
       </ul>
     </nav>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,6 +4,12 @@ import Template from '../layouts/template';
 const ContactPage = () => (
   <div>
     <Template>
+      <h1>Management & Booking</h1>
+      <p>Jorge Jordan</p>
+      <a href="mailto:jordan@lavergeband.com">jordan@lavergeband.com</a>
+      <h1>Press & Communication</h1>
+      <p>Paloma Medina</p>
+      <a href="mailto:paloma@lavergeband.com">paloma@lavergeband.com</a>
     </Template>
   </div>
 )
