ac23ccb55a4215a496f6e1a4975bc9fcfde600fe
victormoya <vmoyacueto@gmail.com>
2018-11-18 19:58:03 +0100

    Updated contact info

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,14 +2,14 @@ import React from 'react'
 import Template from '../layouts/template';
 
 const ContactPage = () => (
-  <div>
+  <div style={{textAlign: "center"}}>
     <Template>
-      <h1>Management & Booking</h1>
-      <p>Jorge Jordan</p>
+      <h1>Management</h1>
+      <p>Jorge Jordan +34 696 164 666</p>
       <a href="mailto:jordan@lavergeband.com">jordan@lavergeband.com</a>
-      <h1>Press & Communication</h1>
-      <p>Paloma Medina</p>
-      <a href="mailto:paloma@lavergeband.com">paloma@lavergeband.com</a>
+      <h1>Booking</h1>
+      <p>Ignacio Aznar +34 670 763 759</p>
+      <a href="mailto:info@carcosarecords.com">info@carcosarecords.com</a>
     </Template>
   </div>
 )
